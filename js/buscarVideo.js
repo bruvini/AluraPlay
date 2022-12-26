c06fafe1f8f8cacbc1466c0451b71fd14710b9cb
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-26 11:26:36 -0300

    buscando elementos

---
+++ js/buscarVideo.js
@@ -0,0 +1,27 @@
+import { conectaAPI } from "./conectaAPI.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscaVideo(evento) {
+    evento.preventDefault()
+
+    const dadosPesquisa = document.querySelector("[data-pesquisa]").value
+    const busca = await conectaAPI.buscaVideo(dadosPesquisa)
+
+    const lista = document.querySelector("[data-lista]")
+
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild)
+    }
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(
+        elemento.titulo,
+        elemento.descricao,
+        elemento.url,
+        elemento.imagem
+    )
+    )
+    );
+}
+
+const botaoPesquisa = document.querySelector("[data-botao-pesquisa]")
+botaoPesquisa.addEventListener("click", evento => buscaVideo(evento))
--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -4,6 +4,32 @@ async function listaVideos() {
     return conexaoConvertida
 }
 
+async function criaVideos(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST", 
+        headers: {
+            "Content-type": "application/json"
+        }, 
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    } )
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
 export const conectaAPI = {
-    listaVideos
+    listaVideos, 
+    criaVideos,
+    buscaVideo
 }
